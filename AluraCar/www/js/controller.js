268dc44de332178ebac2bf29a13ac18915c3eef8
Paulo-Sergio <paulofranca.contato@gmail.com>
2016-07-30 12:21:45 -0300

    acrescentando acessorios ao carro escolhido (ng-model e passando parametros da view para controller.js) --aula05 pt.2

--- AluraCar/www/js/controller.js
+++ AluraCar/www/js/controller.js
@@ -23,4 +23,18 @@ angular.module('starter')
     //$stateParams -> recebendo os parametros :carro
     $scope.carroEscolhido = angular.fromJson($stateParams.carro);
 
+    $scope.listaDeAcessorios = [
+        {"nome":"Freio ABS", "preco":800},
+        {"nome":"Ar Conficionado", "preco":1000},
+        {"nome":"MP3 Player", "preco":500}
+    ];
+
+    $scope.mudou = function(acessorio, isMarcado){
+        if(isMarcado){
+            $scope.carroEscolhido.preco = $scope.carroEscolhido.preco + acessorio.preco;
+        }else{
+            $scope.carroEscolhido.preco = $scope.carroEscolhido.preco - acessorio.preco;
+        }
+    };
+
 });
