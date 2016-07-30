5149e494a5c274b5b1060f129d08e22a3a44ef75
Paulo-Sergio <paulofranca.contato@gmail.com>
2016-07-30 11:21:16 -0300

    trabalhando com rotas e templates no ionic -- aula 04 pt.1

---
+++ AluraCar/www/js/routes.js
@@ -0,0 +1,15 @@
+angular.module('starter')
+.config(function($stateProvider, $urlRouterProvider){
+
+    //configurando rota padrão
+    $urlRouterProvider.otherwise('/listagem');
+
+    $stateProvider
+
+    .state('listagem', {
+        url : '/listagem',
+        templateUrl : 'templates/listagem.html',
+        contoller : 'ListagemController'
+    })
+
+})
